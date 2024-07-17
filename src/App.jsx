ae56a2e791a91f8860ccc84432e9c50e0b7b2e25
Alima <alimausmani21@navgurukul>
2024-07-17 19:23:24 +0530

    Navbar Compited With Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Events from './pages/Events/Events';
+import Gallery from './pages/Gallery/Gallery';
+import Contact from './pages/Contact/Contact';
+import Login from './pages/Login/Login';
+import './index.css';
 
 function App() {
-
   return (
-    <>
-      <div>Hello World</div>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} /> {/* Default route */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Gallery/Gallery.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Gallery = () => {
+  return (
+    <div>
+      <h1>Gallery Page</h1>
+      <p>Explore our gallery.</p>
+    </div>
+  );
+};
+
+export default Gallery;
